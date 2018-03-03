ff824ab889e1248a693bd9fd8c3179e27fdfea55
maxpopovv <max.popovv@gmail.com>
2018-03-03 06:28:02 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
 
+/**
+* Class Calculate решение задачи части 001 урок1.
+* @author popov  
+* @since 03.02.2018
+*/
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
