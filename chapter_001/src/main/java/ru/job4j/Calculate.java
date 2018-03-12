8000fdc6263dc9deea2958d905b2f2794767230b
maxpopovv <max.popovv@gmail.com>
2018-03-12 23:52:40 +0300

    1.3 JavaDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -6,6 +6,11 @@ package ru.job4j;
 * @since 03.02.2018
 */
 
+/**
+* Main 
+* @param args - args
+*/
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
