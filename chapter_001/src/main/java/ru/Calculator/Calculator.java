77eb11ce4a863c4b7c7c9a56db24d32edb65e89e
maxpopovv <max.popovv@gmail.com>
2018-06-15 23:39:37 +0300

    first

---
+++ chapter_001/src/main/java/ru/Calculator/Calculator.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+/**
+ * Class Calculate1 создание элементарного калькулятора.
+ * @author popov
+ * @since 15.06.2018
+ */
+
+
+
+public class Calculator {
+    private double result;
+
+    public void add(double first, double second){
+        this.result = first + second;
+    }
+    public double getResult() {
+        return this.result;
+    }
+
+    public void subtract(double first, double second){
+        this.result = first - second;
+    }
+    public void div(double first, double second){
+        this.result = first/second;
+    }
+    public void multiple(double first, double second){
+
+    }
+}
