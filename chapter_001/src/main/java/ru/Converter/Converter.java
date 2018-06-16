ddf7d678ca51ff393549a1f36e14783874e525b1
maxpopovv <max.popovv@gmail.com>
2018-06-16 19:27:44 +0300

    first

--- chapter_001/src/main/java/ru/Converter/Converter.java
+++ chapter_001/src/main/java/ru/Converter/Converter.java
@@ -7,34 +7,41 @@ public class Converter {
 
     /**
      * Конвертируем рубли в евро.
+     *
      * @param value рубли.
      * @return Евро.
      */
-    public int rubleToEuro(int value) {
-        return value/70;
+    public int RubleToEuro(int value) {
+        return value / 70;
     }
 
     /**
      * Конвертируем рубли в доллары
+     *
      * @param value рубли.
      * @return доллары.
      */
-    public int rubleToDollar(int value) {
-        return value/60;
+    public int RubleToDollar(int value) {
+        return value / 60;
     }
+
     /**
      * Конвертируем доллары в рубли.
+     *
      * @param value доллары.
      * @return Рубли
      */
-    public int DollarToRubels(int value) {
-        return 60*value;
+    public int DollarToRuble(int value) {
+        return 60 * value;
     }
+
     /**
      * Конвертируем евро в рубли.
+     *
      * @param value евро.
      * @return Рубли
      */
-    public int EuroToRubels(int value) {
-        return 70*value;
+    public int EuroToRuble(int value) {
+        return 70 * value;
+    }
 }
