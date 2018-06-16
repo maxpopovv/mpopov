848ed461dec6cc1fb152d9d43e60e68fc48dd515
maxpopovv <max.popovv@gmail.com>
2018-06-16 19:18:15 +0300

    first

--- chapter_001/src/main/java/ru/Calculator/Calculator.java
+++ chapter_001/src/main/java/ru/Calculator/Calculator.java
@@ -14,21 +14,11 @@ public class Calculator {
     public void add(double first, double second){
         this.result = first + second;
     }
-    public double getResult() {
-        return this.result;
-    }
-
     public void subtract(double first, double second){
         this.result = first - second;
     }
-    public double getResult() {
-        return this.result;
-    }
     public void div(double first, double second) {
         this.result = first / second;
-    }
-    public double getResult() {
-        return this.result;
     }
     public void multiple(double first, double second) {
         this.result = first * second;
